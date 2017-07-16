58128132cae49ac19998e36b4f208fa06336169c
BAAMOSK <bunth667@gmail.com>
2017-07-16 12:16:25 -0400

    something wrong with state

--- actions/job_actions.js
+++ actions/job_actions.js
@@ -1,5 +1,35 @@
 import axios from 'axios';
+import reverseGeocode from 'latlng-to-zip';
+import qs from 'qs';
 
 import {
   FETCH_JOBS
 } from './types';
+
+const JOB_ROOT_URL = 'http://api.indeed.com/ads/apisearch?';
+const JOB_QUERY_PARAMS = {
+  publisher: '4201738803816157',
+  format: 'json',
+  v: '2',
+  latlong: 1,
+  radius: 10,
+  q: 'javascript'
+};
+
+const buildJobsUrl = (zipcode) => {
+  const query = qs.stringify({ ...JOB_QUERY_PARAMS, l: zipcode });
+  return `${JOB_ROOT_URL}${query}`;
+};
+
+export const fetchJobs = (region, callback) => async dispatch => {
+  try {
+    // let zipcode = await reverseGeocode(region);
+    let zipcode = '33076'
+    const url = buildJobsUrl(zipcode);
+    let { data } = await axios.get(url);
+    dispatch({ type: FETCH_JOBS, payload: data });
+    callback();
+  } catch(err) {
+    console.log(err);
+  }
+};
