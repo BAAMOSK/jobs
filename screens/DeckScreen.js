c9e32bc4198966361d5acc35bee5b2d877d8a8c4
BAAMOSK <bunth667@gmail.com>
2017-07-17 03:37:25 -0400

    added review jobs

--- screens/DeckScreen.js
+++ screens/DeckScreen.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import { connect } from 'react-redux';
 import { MapView } from 'expo';
 import { Card, Button } from 'react-native-elements';
 import Swipe from '../src/components/Swipe';
+import * as actions from '../actions';
 
 class DeckScreen extends Component {
 
   renderCard(job) {
+    const initialRegion = {
+      longitude: job.longitude,
+      latitude: job.latitude,
+      latitudeDelta: 0.045,
+      longitudeDelta: 0.02
+    }
     return (
       <Card title={job.jobtitle} >
+
+        <View style={{ height: 300 }}>
+          <MapView
+              scrollEnabled={false}
+              style={{ flex: 1 }}
+              cacheEnabled={Platform.OS === 'android' ? true : false}
+              initialRegion={initialRegion}
+          >
+          </MapView>
+        </View>
 
         <View style={styles.detailWrapper}>
           <Text>{job.company}</Text>
@@ -32,11 +49,13 @@ class DeckScreen extends Component {
 
   render() {
     return (
-      <View>
+      <View style={{ marginTop: 10 }}>
         <Swipe
           data={this.props.jobs}
           renderCard={this.renderCard}
           renderNoMoreCards={this.renderNoMoreCards}
+          onSwipeRight={job => this.props.likeJob(job)}
+          keyProp="jobkey"
         />
       </View>
     );
@@ -51,8 +70,8 @@ const styles = {
   }
 };
 
-function mapStateToProps = ({ jobs }) {
+function mapStateToProps({ jobs }) {
   return { jobs: jobs.results };
 }
 
-export default connect(mapStateToProps)(DeckScreen);
+export default connect(mapStateToProps, actions)(DeckScreen);
