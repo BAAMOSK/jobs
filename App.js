19c64f87e2aafe3188fc6cbd5d982b2cecf9b905
BAAMOSK <bunth667@gmail.com>
2017-07-17 06:20:22 -0400

    finished push notifications

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
+import { Notifications } from 'expo';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Alert } from 'react-native';
 import { TabNavigator, StackNavigator } from 'react-navigation';
 import { Provider } from 'react-redux';
 
+import registerForNotifications from './services/push_notifications';
 import store from './store';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
@@ -13,6 +15,21 @@ import SettingsScreen from './screens/SettingsScreen';
 
 
 export default class App extends React.Component {
+  componentDidMount() {
+    registerForNotifications();
+    Notifications.addListener((notification) => {
+      const { data: { text}, origin } = notification;
+      //const text = notifications.data.text;
+      if (origin === 'received' && text) {
+        Alert.alert(
+          'New Push Notification',
+          text,
+          [{ text: 'Ok.' }]
+        );
+      }
+    });
+  }
+
   render() {
     const MainNavigator = TabNavigator({
       welcome: { screen: WelcomeScreen },
